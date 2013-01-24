8a7c3f276300110a89863f4db83db6de535e3166
Anton Belousov <erbol.turburgaev@gmail.com>
2013-01-24 06:10:18 +0400

    Bug fixed.

--- protected/commands/AutomationCommand.php
+++ protected/commands/AutomationCommand.php
@@ -319,7 +319,7 @@ class AutomationCommand extends ConsoleCommand
 
             $tablePos = strpos($check->result, '<' . ResultTable::TAG_MAIN);
 
-            if ($tablePos != false)
+            if ($tablePos !== false)
             {
                 $check->table_result = substr($check->result, $tablePos);
                 $check->result = substr($check->result, 0, $tablePos);
