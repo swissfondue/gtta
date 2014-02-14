33ef17268c6a01707ab62bdb102a07f09a5492c3
Anton Belousov <anton@stellarbit.com>
2014-02-14 05:43:39 +0400

    GTTA-133

---
+++ protected/migrations/m140214_013058_21.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Migration m140214_013058_21
+ */
+class m140214_013058_21 extends CDbMigration {
+    /**
+     * Up migration
+     * @return bool
+     */
+    public function safeUp() {
+        $this->insert("packages", array(
+            "name" => "telnet_bruteforce",
+            "type" => Package::TYPE_SCRIPT,
+            "version" => "1.9",
+            "system" => true,
+            "status" => Package::STATUS_INSTALLED
+        ));
+
+        return true;
+	}
+
+    /**
+     * Down migration
+     * @return bool
+     */
+    public function safeDown() {
+        $this->delete("packages", "name = :name AND type = :type AND system", array(
+            "name" => "telnet_bruteforce",
+            "type" => Package::TYPE_SCRIPT
+        ));
+
+		return true;
+	}
+}
