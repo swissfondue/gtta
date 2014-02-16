6e2f3eed1d952d4567da0d97b581dfd46f9fca55
Anton Belousov <anton@stellarbit.com>
2014-02-16 11:00:27 +0400

    GTTA-149

---
+++ protected/migrations/m140216_063143_22.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Migration m140216_063143_22
+ */
+class m140216_063143_22 extends CDbMigration {
+    /**
+     * Up migration
+     * @return bool
+     */
+    public function safeUp() {
+        $this->insert("packages", array(
+            "name" => "shell",
+            "type" => Package::TYPE_SCRIPT,
+            "version" => "1.10",
+            "system" => true,
+            "status" => Package::STATUS_INSTALLED
+        ));
+
+        return true;
+	}
+
+    /**
+     * Down migration
+     * @return bool
+     */
+    public function safeDown() {
+        $this->delete("packages", "name = :name AND type = :type AND system", array(
+            "name" => "shell",
+            "type" => Package::TYPE_SCRIPT
+        ));
+
+		return true;
+	}
+}
