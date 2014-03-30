73152e77411c0182f70cf90de9f13c2dc15ebf97
Anton Belousov <anton@stellarbit.com>
2014-03-30 08:38:43 +0400

    Do not require demo versions to backup before updating.

--- protected/controllers/UpdateController.php
+++ protected/controllers/UpdateController.php
@@ -38,7 +38,7 @@ class UpdateController extends Controller {
                 $backupTime = new DateTime($system->backup);
             }
 
-            if (!$backupTime || $backupTime < $backupLimit) {
+            if (!$system->demo && (!$backupTime || $backupTime < $backupLimit)) {
                 $forbidMessage = Yii::t(
                     "app",
                     "The system has been backed up more than 24 hours ago. Please download a backup before updating the system."
