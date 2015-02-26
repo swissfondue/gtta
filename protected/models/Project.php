3037104b88c646c9cab4af5db8819fb9c270fe9a
Ruslan Zalalov <devnull@localhost>
2015-02-26 17:38:24 +0300

    GTTA-364

--- protected/models/Project.php
+++ protected/models/Project.php
@@ -93,7 +93,7 @@ class Project extends ActiveRecord implements IVariableScopeObject {
             "modules" => array(self::HAS_MANY, "ProjectGtModule", "project_id"),
             "gtChecks" => array(self::HAS_MANY, "ProjectGtCheck", "project_id"),
             "userHoursAllocated" => array(self::STAT, "ProjectUser", "project_id", "select" => "SUM(hours_allocated)"),
-            "trackedTime" => array(self::STAT, "ProjectTime", "project_id", "select" => "SUM(time)"),
+            "trackedTime" => array(self::STAT, "ProjectTime", "project_id", "select" => "trunc(SUM(time) / 3600)"), // Convert seconds to hours
             "timeRecords" => array(self::HAS_MANY, "ProjectTime", "project_id"),
 		);
 	}
