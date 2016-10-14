cac61404461ace6e352bf64584f5c35c80a64a7f
Ruslan Zalalov <ruslanzalalov@gmail.com>
2016-10-14 16:39:52 +0300

    GTTA-512

---
+++ protected/components/NessusReportManager.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * Class InvalidNessusReportException
+ */
+class InvalidNessusReportException extends Exception {}
+
+/**
+ * Class NessusReportManager
+ */
+class NessusReportManager {
+    /**
+     * XML object
+     * @var null
+     */
+    private $_xmlObj = null;
+
+    /**
+     * Parse nessus report
+     * @param $filepath
+     * @throws Exception
+     * @throws InvalidNessusReportException
+     */
+    public function parse($filepath) {
+        if (!file_exists($filepath)) {
+            throw new Exception("File doesn't exists.");
+        }
+
+        $data = [];
+
+        try {
+            $this->_xmlObj = simplexml_load_file($filepath);
+            $report = $this->_xmlObj->Report[0];
+            $name = Yii::t("app", "N/A");
+
+            if (isset($this->_xmlObj->Report[0]["name"])) {
+                $name = $this->_xmlObj->Report[0]["name"] . PHP_EOL;
+            }
+
+            $data[$name] = $this->_parseHosts($report->ReportHost);
+        } catch (Exception $e) {
+            throw new InvalidNessusReportException();
+        }
+
+        return $data;
+    }
+
+    /**
+     * Parse report's hosts
+     * @param $reportHost
+     * @return array
+     */
+    private function _parseHosts($reportHost) {
+        $hosts = [];
+
+        foreach ($reportHost as $num => $host) {
+            $vulns = $host[0]->ReportItem;
+
+            $hosts[] = [
+                "name" => $host["name"],
+                "properties" => $this->_parseHostProperties($host[0]->HostProperties->children()),
+                "vulnerabilities" => $this->_parseHostVulns($vulns)
+            ];
+        }
+
+        return $hosts;
+    }
+
+    /**
+     * Parse host's properties
+     * @param $hostProperties
+     * @return array
+     */
+    private function _parseHostProperties($hostProperties) {
+        $props = [];
+        $names = [
+            "mac-address",
+            "system-type",
+            "operating-system",
+            "host-ip",
+            "host-fqdn",
+            "netbios-name"
+        ];
+
+        foreach ($hostProperties as $hp) {
+            $attrs = $hp->attributes();
+            $name = (string) $attrs["name"];
+            $value = (string) $hp;
+
+            if (in_array($name, $names)) {
+                $props[$name] = $value;
+            }
+        }
+
+        return $props;
+    }
+
+    /**
+     * Parse host vulnerabilities
+     * @param $hostVulns
+     * @return array
+     */
+    private function _parseHostVulns($hostVulns) {
+        $vulns = [];
+
+        foreach ($hostVulns as $item) {
+            $cvss = $item->cvss_base_score ? (string) $item->cvss_base_score : 0.0;
+            $attributes = $item->attributes();
+
+            $vulns[] = [
+                "pluginName" => (string)$attributes["pluginName"],
+                "svc_name" => (string)$attributes["svc_name"],
+                "severity" => $cvss,
+                "pluginFamily" => (string)$attributes["pluginFamily"],
+                "description" => (string) $item->description,
+                "cve" => (string) $item->cve,
+                "risk_factor" => (string) $item->risk_factor,
+                "see_also" => (string) $item->see_also,
+                "solution" => (string) $item->solution,
+                "synopsis" => (string) $item->synopsis
+            ];
+        }
+
+        return $vulns;
+    }
+}
