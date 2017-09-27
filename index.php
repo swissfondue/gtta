6eb02f5393c8eeb9dd2263e638d7df1ff14473a6
Marat <maratcooL@mail.ru>
2017-09-27 14:20:32 +0300

    GTTA-593_delete_deug_variable

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG',true);
 $common = dirname(__FILE__) . "/protected/common.php";
 $config = dirname(__FILE__) . "/protected/config/main.php";
 $yii = dirname(__FILE__) . "/protected/framework/yii.php";
